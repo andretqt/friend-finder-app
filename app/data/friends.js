b7c83b31ec4157518edfe05e6ba64b1c91606d8c
yung dev <andre.torquati@gmail.com>
2018-07-24 00:41:55 -0400

    the match making works now. Have to do homepage, add more questions to survey, and create a modal popup to show user result

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,15 @@
 var friendsArray = [
     {
-    firstName: 'Bob',
-    score: [4,5,4]
+        firstName: 'Bob',
+        score: ['4','5','4']
+    },
+    {
+        firstName: 'Morty',
+        score: ['5','5','5']
+    },
+    {
+        firstName: 'Michael',
+        score: ['2','2','2']
     }
 
 ]; //array of objects
