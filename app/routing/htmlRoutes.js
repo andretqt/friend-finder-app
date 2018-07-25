a2f1a8ce4477e9660deed1a644eb009d15a84a05
yung dev <andre.torquati@gmail.com>
2018-07-25 01:04:39 -0400

    have to fix catch all routing issue

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, '../public/survey.html'));
        
     });
-    // app.get('*', function(req,res) {
-    //     res.sendFile(path.join(__dirname, 'home.html'));
-    // });
+    app.get('/home', function(req,res) {
+        res.sendFile(path.join(__dirname, '../public/home.html'));
+    });
 };
 
