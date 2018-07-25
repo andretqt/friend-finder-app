c56cc030e54877f9a3d1c7a276ffa88885574faf
yung dev <andre.torquati@gmail.com>
2018-07-24 21:42:01 -0400

    have to do homepage now

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,18 @@
 var friendsArray = [
     {
         firstName: 'Bob',
-        score: ['4','5','4']
+        score: ['4','5','4'],
+        link: '..'
     },
     {
         firstName: 'Morty',
-        score: ['5','5','5']
+        score: ['5','5','5'],
+        link: '..'
     },
     {
         firstName: 'Michael',
-        score: ['2','2','2']
+        score: ['2','2','2'],
+        link: '..'
     }
 
 ]; //array of objects
