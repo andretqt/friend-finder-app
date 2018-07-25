017afd847acd5116b6d27d132ad535f77795d033
yung dev <andre.torquati@gmail.com>
2018-07-25 14:08:21 -0400

    fixed routing issue

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,10 +1,19 @@
 var path = require('path');
+let friends = require('../data/friends');
+
 module.exports = function (app) {
     app.get('/survey', function(req, res){
         res.sendFile(path.join(__dirname, '../public/survey.html'));
        
     });
-    app.get('/', function(req,res) {
+    app.get('/api/friends', function (req, res) {
+        res.json(friends);
+    });
+    app.post('/api/friends',function(req, res){
+        friends.push(req.body);
+        res.json(true);
+    });
+    app.get('*', function(req,res) {
         res.sendFile(path.join(__dirname, '../public/home.html'));
     });
 };
