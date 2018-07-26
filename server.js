3930889d3385b98c97436d2af82bc3f8ede20ea1
yung dev <andre.torquati@gmail.com>
2018-07-26 18:21:34 -0400

    modified port for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
-var PORT = 8080 || process.env.PORT;
+var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 /////
